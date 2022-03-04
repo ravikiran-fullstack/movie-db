b6996496ce0f015551d97ded5a563667881d1632
ravikiran <ravikiransjce.jobs@gmail.com>
2022-03-04 17:51:30 +0530

    Initial setup for movie-db

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
-function App() {
+//f0eb4f1b
+const API_URL = "http://www.omdbapi.com?apikey=5f154763";
+const App = () => {
+  const searchMovies = async (searchTitle) => {
+    const response = await fetch(`${API_URL}&s=${searchTitle}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("superman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MovieDB</h1>
     </div>
   );
-}
+};
 
 export default App;
